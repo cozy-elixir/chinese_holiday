14591e271ace89641636f2b252a38d41e049c9ee
c4710n <c4710n@users.noreply.github.com>
2023-01-21 20:10:01 +0800

    chore: prepare for publish

--- mix.exs
+++ mix.exs
@@ -1,13 +1,23 @@
 defmodule ChineseHoliday.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Provides utilities for handling chinese holiday related problems."
+  @source_url "https://github.com/cozy-elixir/chinese_holiday"
+
   def project do
     [
       app: :chinese_holiday,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: @description,
+      source_url: @source_url,
+      homepage_url: @source_url,
+      docs: docs(),
+      package: package(),
+      aliases: aliases()
     ]
   end
 
@@ -21,7 +31,39 @@ defmodule ChineseHoliday.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, ">= 0.0.0"}
+      {:jason, ">= 0.0.0"},
+      {:ex_check, "~> 0.14.0", only: [:dev], runtime: false},
+      {:credo, ">= 0.0.0", only: [:dev], runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false},
+      {:mix_audit, ">= 0.0.0", only: [:dev], runtime: false}
     ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: @version
+    ]
+  end
+
+  defp package do
+    [
+      exclude_patterns: [],
+      licenses: ["Apache-2.0"],
+      links: %{GitHub: @source_url}
+    ]
+  end
+
+  defp aliases do
+    [publish: ["hex.publish", "tag"], tag: &tag_release/1]
+  end
+
+  defp tag_release(_) do
+    Mix.shell().info("Tagging release as #{@version}")
+    System.cmd("git", ["tag", @version])
+    System.cmd("git", ["push", "--tags"])
   end
 end
