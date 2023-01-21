9066c24a5fdf246759f04a4a1d34f82ff3b1466b
c4710n <c4710n@users.noreply.github.com>
2023-01-21 16:56:12 +0800

    feat: add main modules

--- lib/chinese_holiday.ex
+++ lib/chinese_holiday.ex
@@ -1,18 +1,123 @@
 defmodule ChineseHoliday do
   @moduledoc """
-  Documentation for `ChineseHoliday`.
+  Provides utilities for handling chinese holiday related problems.
   """
 
+  require Logger
+  alias __MODULE__.Data
+
+  @supported_years Data.get_supported_years()
+
   @doc """
-  Hello world.
+  Gets all the supported years.
+  """
+  @spec get_supported_years() :: list()
+  def get_supported_years(), do: @supported_years
+
+  @doc """
+  Checks if a given date is a holiday.
+
+  Following dates will be considered as holidays:
+
+  * dates in holiday schedules which are published by State Council of the People's Republic of China.
+
+  > To get the original information, please visit
+  > [中华人民共和国中央人民政府 - 政府信息公开](http://www.gov.cn/zhengce/xxgk/index.htm),
+  > and search '节假日'.
 
   ## Examples
 
-      iex> ChineseHoliday.hello()
-      :world
+      # the holiday of lunar new year
+      iex> ChineseHoliday.is_holiday?(~D[2023-01-21])
+      true
+
+      # the holiday of lunar new year
+      iex> ChineseHoliday.is_holiday?(~D[2023-01-27])
+      true
+
+      # it's time to work ;)
+      iex> ChineseHoliday.is_holiday?(~D[2023-01-28])
+      false
+
+      # the holiday information is missing, so this function doesn't know how to handle it.
+      # in this case, it will log a warning message, and return `false`.
+      iex> ChineseHoliday.is_holiday?(~D[2090-01-28])
+      false
 
   """
-  def hello do
-    :world
+  @spec is_holiday?(Date.t()) :: boolean()
+  def is_holiday?(%Date{year: year}) when year not in @supported_years do
+    Logger.warning(fn ->
+      "the holiday information of #{year} isn't provided by current version of chinese_holiday"
+    end)
+
+    false
+  end
+
+  Data.get_holidays()
+  |> Enum.map(fn entry ->
+    %{entry | date: Macro.escape(entry.date)}
+  end)
+  |> Enum.each(fn %{date: date} ->
+    def is_holiday?(unquote(date)), do: true
+  end)
+
+  def is_holiday?(_), do: false
+
+  @doc """
+  Checks if a given date is a working day.
+
+  Following dates will be considered as working days:
+
+  * dates marked as additional working days to compensate for the long holiday break.
+  * dates which are normal weekdays (monday ~ friday).
+
+  ## Examples
+
+      # additional working days
+      iex> ChineseHoliday.is_working_day?(~D[2023-01-28])
+      true
+
+      iex> ChineseHoliday.is_working_day?(~D[2023-01-29])
+      true
+
+      # normal weekdays
+      iex> ChineseHoliday.is_working_day?(~D[2023-01-30])
+      true
+
+      iex> ChineseHoliday.is_working_day?(~D[2023-01-31])
+      true
+
+      # normal weekends
+      iex> ChineseHoliday.is_working_day?(~D[2023-02-11])
+      false
+
+      # the working day information is missing, so this function doesn't know how to handle it.
+      # in this case, it will log a warning message, and return `false`.
+      iex> ChineseHoliday.is_working_day?(~D[2090-01-28])
+      false
+
+  """
+  @spec is_working_day?(Date.t()) :: boolean()
+  def is_working_day?(%Date{year: year}) when year not in @supported_years do
+    Logger.warning(fn ->
+      "the working day information of #{year} isn't provided by current version of chinese_holiday"
+    end)
+
+    false
+  end
+
+  Data.get_working_days()
+  |> Enum.map(fn entry ->
+    %{entry | date: Macro.escape(entry.date)}
+  end)
+  |> Enum.each(fn %{date: date} ->
+    def is_working_day?(unquote(date)), do: true
+  end)
+
+  def is_working_day?(%Date{} = date) do
+    %{year: year, month: month, day: day} = date
+    weekday = :calendar.day_of_the_week(year, month, day)
+    weekday in 1..5
   end
 end
---
+++ lib/chinese_holiday/data.ex
@@ -0,0 +1,80 @@
+defmodule ChineseHoliday.Data do
+  @moduledoc false
+
+  def get_supported_years() do
+    get_data()
+    |> Enum.map(& &1.year)
+    |> Enum.reverse()
+  end
+
+  def get_holidays() do
+    get_data()
+    |> Enum.map(& &1.detail)
+    |> Enum.concat()
+    |> Enum.filter(&(&1.type == :holiday))
+    |> Enum.map(fn
+      %{name: name, range: %Date.Range{} = date_range} ->
+        Enum.map(date_range, &%{name: name, date: &1})
+
+      %{name: name, range: %Date{} = date} ->
+        %{name: name, date: date}
+    end)
+    |> List.flatten()
+  end
+
+  def get_working_days() do
+    get_data()
+    |> Enum.map(& &1.detail)
+    |> Enum.concat()
+    |> Enum.filter(&(&1.type == :working_day))
+    |> Enum.map(fn
+      %{name: name, range: %Date.Range{} = date_range} ->
+        Enum.map(date_range, &%{name: name, date: &1})
+
+      %{name: name, range: %Date{} = date} ->
+        %{name: name, date: date}
+    end)
+    |> List.flatten()
+  end
+
+  defp get_data() do
+    get_data_dir()
+    |> Path.join("index.json")
+    |> File.read!()
+    |> Jason.decode!(keys: :atoms!)
+    |> Enum.map(fn %{year: year, last_modified: last_modified} ->
+      %{year: year, last_modified: last_modified, detail: get_year_detail(year)}
+    end)
+  end
+
+  defp get_year_detail(year) do
+    get_data_dir()
+    |> Path.join("#{year}.json")
+    |> File.read!()
+    |> Jason.decode!(keys: :atoms!)
+    |> Enum.map(fn entry ->
+      %{
+        entry
+        | range: transform_range!(entry.range),
+          type: transform_type!(entry.type)
+      }
+    end)
+  end
+
+  defp get_data_dir() do
+    Path.join(:code.priv_dir(:chinese_holiday), "data")
+  end
+
+  defp transform_range!([from, to]) do
+    from = Date.from_iso8601!(from)
+    to = Date.from_iso8601!(to)
+    Date.range(from, to, 1)
+  end
+
+  defp transform_range!([day]) do
+    Date.from_iso8601!(day)
+  end
+
+  defp transform_type!("holiday"), do: :holiday
+  defp transform_type!("workingday"), do: :working_day
+end
